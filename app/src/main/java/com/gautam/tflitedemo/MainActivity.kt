05d802a274a7e6b0431de8802dbe60bcf85645fd
Gautam Sukhramani <gautam.sukhramani87@gmal.com>
2023-01-19 16:51:10 +0530

    add camera permission

--- app/src/main/java/com/gautam/tflitedemo/MainActivity.kt
+++ app/src/main/java/com/gautam/tflitedemo/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.gautam.tflitedemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.CAMERA
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.appcompat.app.AppCompatActivity
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
+
+private const val REQUEST_CODE_CAMERA_PERMISSION = 123
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkCameraPermission()
     }
+
+    @AfterPermissionGranted(REQUEST_CODE_CAMERA_PERMISSION)
+    private fun checkCameraPermission() {
+        if (EasyPermissions.hasPermissions(this, CAMERA)) {
+            // Have permission, do things!
+            Toast.makeText(this, "TODO: Camera things", LENGTH_SHORT).show()
+        } else {
+            // Ask for one permission
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.permission_camera_message),
+                REQUEST_CODE_CAMERA_PERMISSION,
+                CAMERA
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
